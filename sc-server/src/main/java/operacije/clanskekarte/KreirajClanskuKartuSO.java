28865e235ccf9ae739e2a714212463e916344822
milja02 <nikola.miljkovic1234@gmail.com>
2026-06-28 20:09:25 +0200

    Dokumentovana apstraktna genericka operacija i so za clanske karte

--- sc-server/src/main/java/operacije/clanskekarte/KreirajClanskuKartuSO.java
+++ sc-server/src/main/java/operacije/clanskekarte/KreirajClanskuKartuSO.java
@@ -8,14 +8,27 @@ import java.util.Date;
 import java.util.List;
 import operacije.ApstraktnaGenerickaOperacija;
 
+/**
+ * Sistemska operacija za kreiranje praznog zapisa članske karte u bazi.
+ * Ubacuje kartu sa podrazumevanim vrednostima i vraća generisani identifikator.
+ */
 public class KreirajClanskuKartuSO extends ApstraktnaGenerickaOperacija {
 
+    /** Novokreirana članska karta sa generisanim id-jem. */
     private ClanskaKarta clanskaKarta;
 
     @Override
     protected void preduslovi(Object param) throws Exception {
     }
 
+    /**
+     * Ubacuje prazan zapis članske karte u bazu koristeći prvog polaznika
+     * i prvog instruktora iz baze, zatim čuva generisani id.
+     *
+     * @param param parametar operacije (nije korišćen)
+     * @param kljuc dodatni ključ operacije
+     * @throws Exception ako u bazi nema polaznika ili instruktora, ili insert ne uspe
+     */
     @Override
     protected void izvrsiOperaciju(Object param, String kljuc) throws Exception {
         List<ApstraktniDomenskiObjekat> polaznici = broker.getAll(new Polaznik(), null);
@@ -40,6 +53,11 @@ public class KreirajClanskuKartuSO extends ApstraktnaGenerickaOperacija {
         clanskaKarta.setIdClanskaKarta(id);
     }
 
+    /**
+     * Vraća novokreiranu člansku kartu sa generisanim id-jem.
+     *
+     * @return članska karta sa id-jem
+     */
     public ClanskaKarta getClanskaKarta() {
         return clanskaKarta;
     }
