448028abe6496296a716865a4de3cb08512ecad4
milja02 <nikola.miljkovic1234@gmail.com>
2026-06-28 20:15:12 +0200

    Dokumentovane SO za instruktore, licence i mesta

--- sc-server/src/main/java/operacije/instruktori/UcitajInstruktoreSO.java
+++ sc-server/src/main/java/operacije/instruktori/UcitajInstruktoreSO.java
@@ -4,19 +4,35 @@ import domen.Instruktor;
 import java.util.List;
 import operacije.ApstraktnaGenerickaOperacija;
 
+/**
+ * Sistemska operacija za učitavanje svih instruktora iz baze.
+ */
 public class UcitajInstruktoreSO extends ApstraktnaGenerickaOperacija {
 
-    List<Instruktor> instruktori;
+    /** Lista učitanih instruktora. */
+    private List<Instruktor> instruktori;
 
     @Override
     protected void preduslovi(Object param) throws Exception {
     }
 
+    /**
+     * Učitava sve instruktore iz baze podataka.
+     *
+     * @param param parametar operacije (nije korišćen)
+     * @param kljuc dodatni ključ operacije
+     * @throws Exception ako dođe do greške pri radu sa bazom
+     */
     @Override
     protected void izvrsiOperaciju(Object param, String kljuc) throws Exception {
         instruktori = (List<Instruktor>) (List<?>) broker.getAll(new Instruktor(), null);
     }
 
+    /**
+     * Vraća listu učitanih instruktora.
+     *
+     * @return lista instruktora
+     */
     public List<Instruktor> getInstruktori() {
         return instruktori;
     }
--- sc-server/src/main/java/operacije/mesta/UcitajMestaSO.java
+++ sc-server/src/main/java/operacije/mesta/UcitajMestaSO.java
@@ -4,19 +4,35 @@ import domen.Mesto;
 import java.util.List;
 import operacije.ApstraktnaGenerickaOperacija;
 
+/**
+ * Sistemska operacija za učitavanje svih mesta iz baze.
+ */
 public class UcitajMestaSO extends ApstraktnaGenerickaOperacija {
 
+    /** Lista učitanih mesta. */
     private List<Mesto> mesta;
 
     @Override
     protected void preduslovi(Object param) throws Exception {
     }
 
+    /**
+     * Učitava sva mesta iz baze podataka.
+     *
+     * @param param parametar operacije (nije korišćen)
+     * @param kljuc dodatni ključ operacije
+     * @throws Exception ako dođe do greške pri radu sa bazom
+     */
     @Override
     protected void izvrsiOperaciju(Object param, String kljuc) throws Exception {
         mesta = (List<Mesto>) (List<?>) broker.getAll(new Mesto(), null);
     }
 
+    /**
+     * Vraća listu učitanih mesta.
+     *
+     * @return lista mesta
+     */
     public List<Mesto> getMesta() {
         return mesta;
     }
